e802a62ef4a8a3bd3616eb02a1b47b12516dc30f
ulises <sanchezulises952@gmail.com>
2022-12-26 18:57:04 -0300

    cambios

--- moon/src/App.js
+++ moon/src/App.js
@@ -13,40 +13,23 @@ import Borders from './Componets/Borders';
 function App() {
   return <div className='App'>
     <BrowserRouter>
-<<<<<<< HEAD
     <Navbar />
-    <Routes>
-         
-    <Route path='/' element={<Home/>}  />
-
-                      
-       <Route path='products' element={<ItemListContainer/>}>
+  <Routes>
+  <Route path='/' element={<Home/>}/>
+           
+           <Route path='products' element={<ItemListContainer/>}>
             <Route path='category/:id' element={<ItemListContainer/>}  />
             
          </Route>
-=======
-      <Navbar />
-      <MainBackground />
->>>>>>> 0706de21cdb906e993d860a99175f9e971987f03
-
-      <Routes>     
-        <Route path='/' element={<Home/>}  />
-
-        <Route path='products' element={<ItemListContainer/>}>
-          <Route path='category/:id' element={<ItemListContainer/>}  />
-        </Route>
-
-        <Route path='/detail' element={<ItemDetailContainer/>} />
-      </Routes>
+  </Routes>
         
-<<<<<<< HEAD
+
         
      
       
-=======
-      <MidSection />
-      <InstagramGallery />
->>>>>>> 0706de21cdb906e993d860a99175f9e971987f03
+
+    
+
       <Footer />
       <Borders />
     </BrowserRouter>
